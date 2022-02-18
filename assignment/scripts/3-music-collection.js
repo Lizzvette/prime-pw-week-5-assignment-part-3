0d751bdcf80341635d4493ab6ceafabd3d6c50e9
Lizzie Q <lizzieqtech@gmail.com>
2022-02-18 10:38:58 -0600

    Wk5PT3-console.log-collectionarray

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -12,7 +12,7 @@ function addToCollection (title, artist, yearPublished) {
     collection.push(album);
     return album;
 }
-console.log('Pushed album to array: & git');
+console.log('Pushed album to array & git');
 
 /*While working through the features below, make sure to commit after you complete a task! Your commit message should reflect what you added or changed since the previous commit.
 
@@ -34,21 +34,37 @@ console.log(addToCollection('Any', 'Bruno Mars', '2017'));
 console.log(addToCollection('Sugar', 'Maroon 5', '2014'));
 console.log(addToCollection('Any', 'Imagine Dragons', '2014'));
 
-console.log('Pushed collection to git:', collection);
+console.log('Consoled & Pushed collection to git:', collection);
 
 /*-- Add 6 albums to your collection. Aim to have a mix of both same and different artists and published years. (Feel free to share your musical interests, or make stuff up. Totally fine either way.)
 Console.log each album as added using the returned value.
 
-After all are added, console.log the collection array.
+After all are added, console.log the collection array.*/
+function showCollection (array){
+    console.log(array.length);
+    for(let i=0; i<array.length; i++){
+        console.log(`${array[i].title} by ${array[i].artist} published in ${array[i].yearPublished} `);   //TITLE by ARTIST, published in YEAR.
+            
+        }
+    }
+//[i] to pull/display each element from array 
+showCollection(collection );
 
-Add a function named showCollection. This function should:
+/*Add a function named showCollection. This function should:
 
 Take in an array parameter. (This allows it to be reused to show any collection, like the results from the find or search.)
 Console.log the number of items in the array.
 Loop over the array and console.log each album's information formatted like: TITLE by ARTIST, published in YEAR.
-Test the showCollection function.
+Test the showCollection function.*/
 
-Add a function named findByArtist. This function should:
+
+
+//function
+
+
+
+
+/*Add a function named findByArtist. This function should:
 
 Take in artist (a string) parameter
 Create an array to hold any results, empty to start
@@ -58,6 +74,8 @@ Test the findByArtist function. Make sure to test with an artist you know is in 
 
 When testing your functions, write all tests in the JavaScript file!
 
+
+// **********************************************************
 Stretch goals
 Create a function called search. This function should:
 
